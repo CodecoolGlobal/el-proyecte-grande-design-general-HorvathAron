46b3ba03bc82fe77ec03836af419f96f0d24ab95
Imreszab <szabimi12@gmail.com>
2023-03-09 15:44:25 +0100

    add seeders to DatabaseSeeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -12,11 +12,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UsersTableSeeder::class,
+            TagsTableSeeder::class,
+            EventsTableSeeder::class,
+            NewsFeedsTableSeeder::class,
+            ParticipantsTableSeeder::class,
+            EventsTagsTableSeeder::class,
+            ChatMessagesTableSeeder::class
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
