e3d25aa54888711d2f81908218ce1668dafaf2b7
Imreszab <szabimi12@gmail.com>
2023-03-09 15:50:46 +0100

    add fillable array to NewsFeed model

--- backend/app/Models/NewsFeed.php
+++ backend/app/Models/NewsFeed.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class NewsFeed extends Model
 {
     use HasFactory;
+
+    protected $fillable =[
+        "message",
+        "created_by",
+        "event"
+    ];
 }
