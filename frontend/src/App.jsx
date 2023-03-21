cea374b9d71540a9657f709cb117a866763bf5ad
Imreszab <szabimi12@gmail.com>
2023-03-21 13:59:59 +0100

    add google login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
 import React from "react";
-import {Typography} from "@material-ui/core"
+import { useEffect } from "react";
+import { Typography } from "@material-ui/core";
 
 const App = () => {
-     return (
-        <div>
-            <Typography variant="h1">Hellooo!!</Typography>
+	const handleCallbackResponse = (response) => {
+		console.log("Encoded Token: " + response.credential);
+	};
 
-        </div>
-     )
-}
+	useEffect(() => {
+		//global google
+		window.google.accounts.id.initialize({
+			client_id:
+				"488646322950-v3t6bpc8ach5eno89o5h249ae5h23vvl.apps.googleusercontent.com",
+			callback: handleCallbackResponse,
+		});
+		window.google.accounts.id.renderButton(
+			document.getElementById("signInDiv"),
+			{
+				theme: "outline",
+				size: "large",
+			}
+		);
+	}, []);
+
+	return (
+		<div>
+			<Typography variant="h1">Hellooo!!</Typography>
+
+			<div id="signInDiv"></div>
+		</div>
+	);
+};
 export default App;
