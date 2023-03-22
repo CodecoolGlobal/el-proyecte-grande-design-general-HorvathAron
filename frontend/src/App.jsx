09cc4cabeeabd0355fe01275226eeb00979dce84
HorvathAron <horvatharon9431@gmail.com>
2023-03-22 14:57:48 +0100

    refactor navBar and create Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,10 @@
 import Layout from "./Components/Layout";
-import { Route, Routes } from "react-router-dom";
 
 
 export default function App() {
   return (
     <div className="App">
-          <Routes>
-            <Route index element={<Layout />}>
-              
-            </Route>
-          </Routes>
+          <Layout/>
     </div>
   );
 }
