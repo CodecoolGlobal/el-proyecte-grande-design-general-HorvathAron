0130f99ae11dfd8113b79e8319962de372f457e4
TwiceG <g0stron0g@gmail.com>
2023-03-22 12:05:53 +0100

    updated Calendar component for layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,12 @@
 import Layout from "./Components/Layout";
-import { Route, Routes } from "react-router-dom";
-import Calendar from './Components/Calendar';
+
 
 
 
 export default function App() {
   return (
     <div className="App">
-          <Routes>
-            <Route index element={<Layout />}>
-            <Calendar/>
-
-            </Route>
-          </Routes>
+          <Layout />      
     </div>
   );
 }
