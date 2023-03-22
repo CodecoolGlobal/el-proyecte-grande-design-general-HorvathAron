5c9355988ab68a102b8c024a0700120ba878c44e
TwiceG <g0stron0g@gmail.com>
2023-03-22 11:20:49 +0100

    add basic calendar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import NavBar from './Components/NavBar';
+import Calendar from './Components/Calendar';
 
 
 export default function App() {
   return (
     <div>
         <NavBar/>
+        <Calendar/>
     </div>
   );
 }
