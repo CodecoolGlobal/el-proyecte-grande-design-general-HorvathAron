eca6c79a86120007928c5fe15e1c043ebf8520f0
LaGabor <lagaborka@gmail.com>
2023-03-23 14:36:31 +0100

    fix in index.js and Layout.jsx, in index now/events using the Cardlister as view, Layout using Content as view

--- frontend/src/Components/Layout.jsx
+++ frontend/src/Components/Layout.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Content from "./Content";
 import NavBar from "./NavBar";
-import CardLister from "./CardLister"
 const Layout = () => {
   return (
     <>
       <NavBar />
-        <CardLister />
+        <Content />
     </>
   );
 };
